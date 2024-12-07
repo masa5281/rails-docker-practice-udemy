6b06563f7464796b2a52383cb806dd1cc87b73fd
masa5281 <masa.h.5281@gmail.com>
2024-12-07 12:49:16 +0900

    Add: Bootstrapのセットアップが完了

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,3 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
+# @import "bootstrap"と記載すると、gem内のbootstrap.min.jsが読み込まれる。
+# preload；実際にJavaScriptが読み込まれるよりも前にライブラリを読み込む。→ 遅延を避ける事ができる。
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "popper", to: "popper.js", preload: true
