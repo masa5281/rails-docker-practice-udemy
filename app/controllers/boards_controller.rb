f5ca516106fe4dd7091c2629f96838b885dd3f8d
masa5281 <masa.h.5281@gmail.com>
2024-12-13 10:42:16 +0900

    Add: 不正な値が入るのを防ぐためバリデーションの設定

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,13 +6,20 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
   end
 
   def create
-    board = Board.create(board_params) # 引数に保存するパラメーターを渡すことでDBに保存できる。
-    flash[:notice] = "「#{board.title}」の掲示板を作成しました" # flashの任意のキーの値のデータが、次に参照されるまでセッションに保存される。
-    redirect_to board # データ作成時点でidが付与されているため、参照している。
+    board = Board.new(board_params) # 引数に保存するパラメーターを渡すことでDBに保存できる。
+    if board.save # この時点でバリデーションが検証される
+      flash[:notice] = "「#{board.title}」の掲示板を作成しました" # flashの任意のキーの値のデータが、次に参照されるまでセッションに保存される。
+      redirect_to board # データ作成時点でidが付与されているため、参照している。
+    else
+      redirect_to new_board_path, flash: {
+        board: board, # flashでboardオブジェクトを返すことで、newメソッドで入力されたままの値で保持される。
+        error_message: board.errors.full_messages # エラーがあるとerrorsにエラーオブジェクトが格納され、full_messagesで見やすい形で配列にする。
+      }
+    end
   end
 
   def show
@@ -23,7 +30,6 @@ class BoardsController < ApplicationController
 
   def update
     @board.update(board_params)
-
     redirect_to @board
   end
 
