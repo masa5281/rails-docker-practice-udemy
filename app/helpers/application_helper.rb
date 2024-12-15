97afb5b81f8dd661339e00a3d39ec8b2894f7435
masa5281 <masa.h.5281@gmail.com>
2024-12-15 17:23:29 +0900

    Add: ヘッダーメニューの追加 Add: カスタムヘルパーの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def header_link_item(name, path)
+    class_name = 'nav-link'
+    class_name << ' active' if current_page?(path) # 表示されているページと引数のパスが同じであれば、class_nameにactiveを追加する（文字列連結 破壊的）。
+
+    # liタグの中にaタグが埋め込まれ、ヘルパーの呼び出し元に返される。
+    content_tag :li, class: 'nav_item' do
+      link_to name, path, class: class_name
+    end
+  end
 end
