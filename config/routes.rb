ac9d225b949d66002ad2ee9375532ca835df644e
masa5281 <masa.h.5281@gmail.com>
2024-12-15 20:28:55 +0900

    Add: usersとsessionsのルーティングを新規設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/me'
-  get 'home/index'
-  get 'sessions/create'
-  get 'sessions/destory'
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   root 'home#index'
+  resources :users, only: %i[new create]
   resources :boards
   resources :comments, only: %i[create destroy]
 end
