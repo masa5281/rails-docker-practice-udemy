38a18d11bee45965d2ac3d63e40277c46e1f05ef
masa5281 <masa.h.5281@gmail.com>
2024-12-15 21:03:31 +0900

    Add: Userモデルの認証機能を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,16 @@
 #  index_users_on_name  (name) UNIQUE
 #
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :name,
+  presence: true,
+  uniqueness: true,
+  length: { maximum: 16 },
+  format: {
+    width: /\A[a-z0-9]+\z/, # 小文字のアルファベットか数字の0〜9のいずれかが1文字以上続く。
+    message: 'は小文字英数字で入力してください' # 「不正な値です」というデフォルトのエラーメッセージをより明示的にするために指定
+  }
+  validates :password, 
+  length: { minimun: 8 }
 end
