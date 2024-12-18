6cdd5cc20bfcb1ced73437e1e09e4f1ec2107f4d
masa5281 <masa.h.5281@gmail.com>
2024-12-19 06:25:28 +0900

    Add: ログインユーザーの取得とマイページの作成 Refactor: form_withとSessionsControllerの記述を修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,16 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(name: params[:session][:name])
+    if user && user.authenticate(params[:session][:password]) # userがnilの場合 / authenticateが実際にユーザー名とパスワードがマッチしているかを確認している。
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render 'index/home'
+    end
   end
 
-  def destory
+  def destroy
+    session.delete(:user_id) # session情報の削除。sessionオブジェクトのdeleteメソッドで実行。
+    redirect_to root_path
   end
 end
